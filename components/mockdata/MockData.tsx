0ce0f33f4d8bbfef1a9dda26160d7899dc226500
Yulya <iuliiaobr.io@gmail.com>
2023-05-19 08:20:52 +0700

    add mock data for testing

--- components/mockdata/MockData.tsx
+++ components/mockdata/MockData.tsx
@@ -4,12 +4,62 @@ export const MockData = {
     {
       flight_date: '2023-05-18',
       flight_status: 'scheduled',
-      departure: [Object],
-      arrival: [Object],
-      airline: [Object],
-      flight: [Object],
-      aircraft: null,
-      live: null
+      "departure": {
+        "airport": "San Francisco International",
+        "timezone": "America/Los_Angeles",
+        "iata": "SFO",
+        "icao": "KSFO",
+        "terminal": "2",
+        "gate": "D11",
+        "delay": 13,
+        "scheduled": "2019-12-12T04:20:00+00:00",
+        "estimated": "2019-12-12T04:20:00+00:00",
+        "actual": "2019-12-12T04:20:13+00:00",
+        "estimated_runway": "2019-12-12T04:20:13+00:00",
+        "actual_runway": "2019-12-12T04:20:13+00:00"
+    },
+    "arrival": {
+        "airport": "Dallas/Fort Worth International",
+        "timezone": "America/Chicago",
+        "iata": "DFW",
+        "icao": "KDFW",
+        "terminal": "A",
+        "gate": "A22",
+        "baggage": "A17",
+        "delay": 0,
+        "scheduled": "2019-12-12T04:20:00+00:00",
+        "estimated": "2019-12-12T04:20:00+00:00",
+        "actual": null,
+        "estimated_runway": null,
+        "actual_runway": null
+    },
+    "airline": {
+        "name": "American Airlines",
+        "iata": "AA",
+        "icao": "AAL"
+    },
+    "flight": {
+        "number": "1004",
+        "iata": "AA1004",
+        "icao": "AAL1004",
+        "codeshared": null
+    },
+    "aircraft": {
+       "registration": "N160AN",
+       "iata": "A321",
+       "icao": "A321",
+       "icao24": "A0F1BB"
+    },
+    "live": {
+        "updated": "2019-12-12T10:00:00+00:00",
+        "latitude": 36.28560000,
+        "longitude": -106.80700000,
+        "altitude": 8846.820,
+        "direction": 114.340,
+        "speed_horizontal": 894.348,
+        "speed_vertical": 1.188,
+        "is_ground": false
+    }
     },
     {
       flight_date: '2023-05-18',
