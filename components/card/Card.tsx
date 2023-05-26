0bfcadd70373e48b86b5be0abd393b938bdd427a
Yulya <iuliiaobr.io@gmail.com>
2023-05-26 19:27:38 +0700

    add card basic layout

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -36,47 +36,42 @@ type SwitchesCardProps = {
 export function SwitchesCard({ data }: SwitchesCardProps) {
   const { classes } = useStyles();
 
-  const items = data.map((item) => (
-    <Group position="apart" key={item.id} className={classes.item} noWrap spacing="xl">
-      <div>
-        <Text>{item.price.grandTotal}</Text>
-        <Text size="xs" color="dimmed">
-          {/* {item.} */}
-        </Text>
-      </div>
-      {/* <Switch onLabel="ON" offLabel="OFF" className={classes.switch} size="lg" /> */}
-    </Group>
-  ));
+  const offers = data.map((item) => (
+    <Card shadow="xs" style={{ maxWidth: '100%', padding: '0 1rem 1rem 1rem', marginBottom: '1rem'}} key={item.id}>
+    <Title>
+      <Text style={{ marginTop: '0.5rem', fontWeight: 'bold', fontSize: '2rem', zIndex: 100 }}>
+        {item.price.total} $
+      </Text>
+    </Title>
+  <Grid gutter="lg">
+    <Col span={3}>
+      <Text weight={500}>Departure</Text>
+      <Text>SYD</Text>
+      <Text weight={500}>12:35 PM</Text>
+    </Col>
+    <Col span={3}>
+      <Text weight={500}>Arrival</Text>
+      <Text>SIN</Text>
+      <Text weight={500}>6:55 PM</Text>
+    </Col>
+    <Col span={3}>
+      <Text weight={500}>Duration</Text>
+      <Text>8h 20m</Text>
+    </Col>
+    <Col span={3}>
+      <Text weight={500}>Airline</Text>
+      <Text>TR</Text>
+      <Text weight={500}>Flight No.</Text>
+      <Text>3</Text>
+    </Col>
+  </Grid>
+</Card>
+  ))
+
 
   return (
-    <Card shadow="xs" style={{ maxWidth: '100%', padding: '0 1rem'}}>
-        <Title>
-          <Text style={{ marginTop: '0.5rem', fontWeight: 'bold', fontSize: '2rem', zIndex: 100 }}>
-            €307.00
-          </Text>
-        </Title>
-      <Grid gutter="lg">
-        <Col span={3}>
-          <Text weight={500}>Departure</Text>
-          <Text>SYD</Text>
-          <Text weight={500}>12:35 PM</Text>
-        </Col>
-        <Col span={3}>
-          <Text weight={500}>Arrival</Text>
-          <Text>SIN</Text>
-          <Text weight={500}>6:55 PM</Text>
-        </Col>
-        <Col span={3}>
-          <Text weight={500}>Duration</Text>
-          <Text>8h 20m</Text>
-        </Col>
-        <Col span={3}>
-          <Text weight={500}>Airline</Text>
-          <Text>TR</Text>
-          <Text weight={500}>Flight No.</Text>
-          <Text>3</Text>
-        </Col>
-      </Grid>
-    </Card>
+    <div>
+    {offers}
+    </div>
   );
 }
